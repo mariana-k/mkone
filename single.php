1f65a7a5b56eca4bad0a0b24a6278051602c6a6e
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2022-11-22 18:08:11 +0200

    Added templates for posts

---
+++ single.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage MKOne    
+ * @since MKOne 1.0
+ */
+ 
+get_header(); ?>
+ 
+    <div class="content-area">
+        <main class="site-main" role="main">
+ 
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+            get_template_part( 'template-parts/content/content-single' );
+        // End the loop.
+        endwhile;
+        ?>
+ 
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
+ 
+<?php get_footer(); ?>
