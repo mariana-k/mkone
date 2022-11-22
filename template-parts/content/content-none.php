867bee2312db72c8528de9374f967b2f2f343126
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2022-11-22 17:15:31 +0200

    Created a basic theme draft

---
+++ template-parts/content/content-none.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage MKOne
+ * @since MKOne 1.0
+ */
+
+?>
+<section class="no-results">
+    <p><?php esc_html_e( 'Nothing here', 'mkone' ); ?></p>
+</section>
